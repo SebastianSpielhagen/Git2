0eb9733f6fc1a385f7904f141bb0a6c064cb1404
SebastianSpielhagen <ssp.java@icloud.com>
2023-11-20 16:40:31 +0100

    add timeline

--- src/main/java/org/spielhagen/Main.java
+++ src/main/java/org/spielhagen/Main.java
@@ -2,6 +2,13 @@ package org.spielhagen;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("TimeLine");
+
+
+
         System.out.println("Hello world!");
+
+
+
     }
 }
